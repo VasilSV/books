41277c31426bb31e8313de71fb917e9fbe453a76
VasilSV <114056331+VasilSV@users.noreply.github.com>
2023-03-05 11:30:51 +0200

    new

---
+++ src/main/java/com/example/books/model/entity/AuthorEntity.java
@@ -0,0 +1,49 @@
+package com.example.books.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+public class AuthorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany (mappedBy = "author")
+    private List<BookEntity> books;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<BookEntity> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<BookEntity> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
